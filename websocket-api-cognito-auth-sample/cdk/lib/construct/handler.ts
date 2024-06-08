7439ca6af415002877ceef54301c785ed641399d
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-06-08 11:52:14 -0700

    upgrade websocket apigateway code base

--- websocket-api-cognito-auth-sample/cdk/lib/construct/handler.ts
+++ websocket-api-cognito-auth-sample/cdk/lib/construct/handler.ts
@@ -20,7 +20,7 @@ export class Handler extends Construct {
     super(scope, id);
 
     const authHandler = new NodejsFunction(this, "AuthHandler", {
-      runtime: Runtime.NODEJS_18_X,
+      runtime: Runtime.NODEJS_20_X,
       entry: "../backend/authorizer/index.ts",
       environment: {
         USER_POOL_ID: props.userPool.userPoolId,
@@ -29,11 +29,11 @@ export class Handler extends Construct {
       bundling: {
         minify: false,
         // esbuildArgs: { '--platform': 'browser' },
-      }
+      },
     });
 
     const websocketHandler = new lambdanode.NodejsFunction(this, "WebSocketHandler", {
-      runtime: Runtime.NODEJS_18_X,
+      runtime: Runtime.NODEJS_20_X,
       entry: "../backend/websocket/index.ts",
       environment: {
         CONNECTION_TABLE_NAME: props.connectionIdTable.tableName,
